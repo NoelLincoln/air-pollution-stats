6e62a61a749d83c637ef94487e6022ab1d68564d
Noel Bryant <noelsobryant@gmail.com>
2023-10-25 20:05:32 +0300

    add onclick handler to state and dispatch fetchGeolocation thunk

--- src/components/StateList.js
+++ src/components/StateList.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchStatesAsync } from '../redux/features/countrySlice';
+import {
+  fetchStatesAsync,
+  fetchGeolocationAsync,
+} from '../redux/features/countrySlice';
 
 const StateList = () => {
   const { countryShortCode, image, states, fetchStatus, error } = useSelector(
@@ -14,20 +17,43 @@ const StateList = () => {
     }
   }, [countryShortCode, dispatch]);
 
+  const handleStateClick = async (state) => {
+    const stateName = state.name;
+
+    if (stateName) {
+      dispatch(fetchGeolocationAsync(stateName));
+    }
+  };
+
   return (
     <section className="state-list-container">
       <h4>Stats By State</h4>
       <ul className="state-list">
         {fetchStatus === 'loading' && <p>Loading states...</p>}
-        {fetchStatus === 'failed' && <p>Error: {error}</p>}
+        {fetchStatus === 'failed' && (
+          <p>
+            Error:
+            {error}
+          </p>
+        )}
         {fetchStatus === 'fulfilled' &&
           states.map((state) => (
             <li key={state.id} className="state-stat">
-              <img src={image} alt="" className="state-img" />
-              <div>
-                <p>{state.name}</p>
-                <p>{state.population}</p>
-              </div>
+              <button
+                type="button"
+                onClick={() => handleStateClick(state)}
+                style={{
+                  border: 'none',
+                  background: 'none',
+                  cursor: 'pointer',
+                }}
+              >
+                <img src={image} alt="" className="state-img" />
+                <div>
+                  <p>{state.name}</p>
+                  <p>{state.population}</p>
+                </div>
+              </button>
             </li>
           ))}
       </ul>
