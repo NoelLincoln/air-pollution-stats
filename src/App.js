6b16fc08c6c2d9aab46c79690f7fb269ab28158e
Noel Bryant <noelsobryant@gmail.com>
2023-10-25 23:17:38 +0300

    set route paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './components/HomePage';
 import StateList from './components/StateList';
+import StateDetails from './components/StateDetails';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      <StateList />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={(
+            <>
+              <HomePage />
+              <StateList />
+            </>
+          )}
+        />
+        <Route path="/state/:id" element={<StateDetails />} />
+      </Routes>
     </div>
   );
 }
