4ffa14e30704b75b2f8c7aff5b727316209707a0
Noel Bryant <noelsobryant@gmail.com>
2023-10-25 03:48:17 +0300

    on page load get user location/country and set country flag

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,47 @@
-import africa from '../images/africa.png';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  setLocation,
+  reverseGeocodeAsync,
+} from '../redux/features/countrySlice';
 
-const HomePage = () => (
-  <section className="home-container">
-    <div className="continet">
-      <div className="continent-img">
-        <img src={africa} alt="" />
-        <div className="continent-details">
-          <h1>Africa</h1>
-          <span>4000 views</span>
+const HomePage = () => {
+  const { country, image } = useSelector((state) => state.country);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    function successCallback(position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      dispatch(setLocation({ latitude, longitude }));
+      dispatch(reverseGeocodeAsync({ latitude, longitude }));
+    }
+
+    function errorCallback(error) {
+      console.error(`Error getting user's location: ${error.message}`);
+    }
+
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    } else {
+      console.error('Geolocation is not available in this browser.');
+    }
+  }, [dispatch]);
+
+  return (
+    <section className="home-container">
+      <div className="continent">
+        <div className="continent-img">
+          <img src={image} alt="" />
+          <div className="continent-details">
+            <h1>{country}</h1>
+            <span>4000 views</span>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default HomePage;
