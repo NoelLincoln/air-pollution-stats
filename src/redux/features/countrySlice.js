172b584847e9015fb4273b2dcab0e86e582fcdb9
Noel Bryant <noelsobryant@gmail.com>
2023-10-27 01:02:34 +0300

    feat: search by country and display states

--- src/redux/features/countrySlice.js
+++ src/redux/features/countrySlice.js
@@ -1,7 +1,27 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, createAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 import countries from '../../components/countries';
+
+export const fetchCountriesAsync = createAsyncThunk(
+  'country/fetchCountries',
+  async () => {
+    const apiKey = 'RWJoWUt3SUtEQXNDSktpUDhIY1VNYTFvTXpSNmxPUjVubUdWV0U3Wg==';
+    const apiEndpoint = 'https://api.countrystatecity.in/v1/countries';
+    const response = await fetch(apiEndpoint, {
+      headers: {
+        'X-CSCAPI-KEY': apiKey,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch states');
+    }
+
+    const data = await response.json();
+    return data;
+  }
+);
 
 export const reverseGeocodeAsync = createAsyncThunk(
   'country/reverseGeocode',
@@ -47,7 +67,6 @@ export const fetchStatesAsync = createAsyncThunk(
     }
 
     const data = await response.json();
-
     return data;
   }
 );
@@ -108,16 +127,22 @@ export const fetchAirQualityAsync = createAsyncThunk(
     }
   }
 );
+export const setCountry = createAction('country/setCountry');
+export const setStates = createAction('country/setStates');
+export const clearCountry = createAction('country/clearCountry');
+export const setCountryShortCode = createAction('country/setCountryShortCode');
 
 const countrySlice = createSlice({
   name: 'country',
   initialState: {
     latitude: null,
     longitude: null,
+    countries: [],
     country: null,
     countryShortCode: '',
     image: null,
     states: [],
+    totalNumberOfStates: null,
     selectedState: [],
     airQualityData: null,
     isDataFetched: false,
@@ -136,6 +161,21 @@ const countrySlice = createSlice({
     setDataFetched: (state, action) => {
       state.isDataFetched = action.payload;
     },
+    setTotalNumberOfStates: (state, action) => {
+      state.totalNumberOfStates = action.payload;
+    },
+    setCountriesList: (state, action) => {
+      state.countries = action.payload;
+    },
+    setCountry: (state, action) => {
+      state.country = action.payload;
+    },
+    clearCountry: (state) => {
+      state.country = null;
+    },
+    setCountryShortCode: (state, action) => {
+      state.countryShortCode = action.payload;
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -152,9 +192,21 @@ const countrySlice = createSlice({
         state.fetchStatus = 'failed';
         state.error = action.error.message;
       })
+      .addCase(fetchCountriesAsync.fulfilled, (state, action) => {
+        state.fetchStatus = 'fulfilled';
+        state.countries = action.payload;
+      })
+      .addCase(fetchCountriesAsync.pending, (state) => {
+        state.fetchStatus = 'loading';
+      })
+      .addCase(fetchCountriesAsync.rejected, (state, action) => {
+        state.fetchStatus = 'failed';
+        state.error = action.error.message;
+      })
       .addCase(fetchStatesAsync.fulfilled, (state, action) => {
         state.fetchStatus = 'fulfilled';
         state.states = action.payload;
+        state.totalNumberOfStates = action.payload.length;
       })
       .addCase(fetchStatesAsync.pending, (state) => {
         state.fetchStatus = 'loading';
@@ -175,6 +227,7 @@ const countrySlice = createSlice({
         state.fetchStatus = 'failed';
       })
       .addCase(fetchAirQualityAsync.fulfilled, (state, action) => {
+        state.fetchStatus = 'fulfilled';
         state.airQualityData = action.payload;
       })
       .addCase(fetchAirQualityAsync.pending, (state) => {
@@ -183,10 +236,19 @@ const countrySlice = createSlice({
       .addCase(fetchAirQualityAsync.rejected, (state, action) => {
         state.fetchStatus = 'failed';
         state.error = action.error.message;
+      })
+      .addCase(setStates, (state, action) => {
+        state.states = action.payload;
       });
   },
 });
 
-export const { setLocation, setSelectedState, setDataFetched } =
-  countrySlice.actions;
+export const {
+  setLocation,
+  setSelectedState,
+  setDataFetched,
+  setTotalNumberOfStates,
+  setSelectedCountry,
+  setCountriesList,
+} = countrySlice.actions;
 export default countrySlice.reducer;
