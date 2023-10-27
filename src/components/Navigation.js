e77aa7432bae611082c5429859b0f8328a6d25a9
Noel Bryant <noelsobryant@gmail.com>
2023-10-27 05:25:53 +0300

    conditionally show back button

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,32 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import arrowback from '../images/arrow-back.png';
 import menuicon from '../images/menu-icon.png';
 
-const Navigation = () => (
-  <div className="nav">
-    <div className="arrow-back">
-      <img src={arrowback} alt="" />
+const Navigation = () => {
+  const location = useLocation();
+
+  const isHomepage = location.pathname === '/';
+
+  return (
+    <div className="nav">
+      {isHomepage ? (
+        <div className="empty-div" />
+      ) : (
+        <div className="arrow-back">
+          <Link to="/">
+            <img src={arrowback} alt="Go Back" />
+          </Link>
+        </div>
+      )}
+      <div>
+        <h2>Air Pollution Stats</h2>
+      </div>
+      <div className="menu-icon">
+        <img src={menuicon} alt="Menu" />
+      </div>
     </div>
-    <div className="menu-icon">
-      <img src={menuicon} alt="" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Navigation;
